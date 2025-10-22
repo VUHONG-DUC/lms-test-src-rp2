8204a2181a9e2da26a64efedb694829f35e7da25
d-vuhong <d-vuhong@3sss.co.jp>
2025-10-22 13:22:18 +0900

    add:Case01、02テストコード追加 結合テスト仕様書追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,7 +36,14 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// LMSへアクセス
+		goTo("http://localhost:8080/lms/");
+		//タイトル取得し、検証を行う
+		String pageTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
